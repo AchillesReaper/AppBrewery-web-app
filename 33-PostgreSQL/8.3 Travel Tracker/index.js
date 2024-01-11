eca8f3bf3ff0b6eb5c639690e7f381e77b066e34
Donald Ho <dh3coding@gmail.com>
2024-01-12 03:32:12 +1100

    section 33 Travel Tracker error handling done

--- 33-PostgreSQL/8.3 Travel Tracker/index.js
+++ 33-PostgreSQL/8.3 Travel Tracker/index.js
@@ -4,6 +4,7 @@ import pg from 'pg'
 const app = express();
 const port = 3000;
 
+let visitedCountries = []
 
 const db = new pg.Client({
   host: 'localhost',
@@ -14,42 +15,54 @@ const db = new pg.Client({
 })
 
 db.connect()
-let visited_countries = []
-
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+async function getVisitedCountries() {
+  // get visited countries from database, return visitedCountries a new array or remain empty 
+  const result = await db.query("SELECT country_code FROM visited_countries")
+  try {
+    visitedCountries = result.rows.map((row) => row.country_code)
+  } catch (err) {
+    console.log(err);
+  }
+}
+
 // GET home page
 app.get("/", async (req, res) => {
-  const result = await db.query("SELECT country_code FROM visited_countries");
-  let countries = result.rows.map((row) => row.country_code)
-  res.render("index.ejs", { countries: countries, total: countries.length });
-  // db.end();
+  getVisitedCountries()
+  res.render("index.ejs", { countries: visitedCountries, total: visitedCountries.length })
 });
 
 //INSERT new country
 app.post('/add', async (req, res) => {
   const input = req.body.country
 
-  console.log(`input = ${input}, type: ${typeof(input)}`);
-
   const qResult = await db.query(
     "select country_code from countries WHERE country_name  = $1",
     [input]
   )
-  console.log(qResult.rows);
-  console.log(`rowCount = ${qResult.rowCount}, type: ${typeof(qResult.rowCount)}`);
 
   if (qResult.rowCount == 1) {
+    getVisitedCountries()
     const countryCode = qResult.rows[0].country_code
-    console.log(countryCode);
-    await db.query(
-      "INSERT INTO visited_countries (country_code) VALUES ($1)",
-      [countryCode]
-    )
+    //check if country_code for the input country is visited
+    const codeIndex = visitedCountries.findIndex((vc) => vc === countryCode)
+    if (codeIndex < 0) {
+      // input not yet exist
+      await db.query(
+        "INSERT INTO visited_countries (country_code) VALUES ($1)",
+        [countryCode]
+      )
+      res.redirect('/');
+    } else {
+      console.log("Conunty has already been added. Try again");
+      res.render('index.ejs', { countries: visitedCountries, total: visitedCountries.length, error: "Conunty has already been added. Try again" })
+    }
+  } else {
+    res.render('index.ejs', { countries: visitedCountries, total: visitedCountries.length, error: "Country name does not exist. Try again" })
   }
-  res.redirect('/');
 
 })
 
